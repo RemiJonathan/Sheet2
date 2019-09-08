468d24f8fab62b64a2b01dca0e750d13c47753c2
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-09-08 03:03:41 -0400

    Controller Throws Errors

--- Sheet2/Controllers/LoanController.cs
+++ Sheet2/Controllers/LoanController.cs
@@ -15,14 +15,14 @@ namespace Sheet2.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(float value, float rate, Int32 years)
+        public ActionResult Index(float value, float rate, float years)
         {
+            //I wanted to add some data validation here and throw the ERRORs into ViewData["Error"] and display it in the view but I couldn't find how to do it.
+            if (!float.TryParse(value.ToString(), out float goodValue) || goodValue >= 0) { ViewData["ValueError"] = "Invalid Value"; return View(); } else { ViewData["Value"] = goodValue; }
+            if (!float.TryParse(rate.ToString(), out float goodRate) || goodRate >= 0) { ViewData["RateError"] = "Invalid Rate"; return View(); } else { ViewData["Rate"] = goodRate; }
+            if (!float.TryParse(years.ToString(), out float goodYears) || goodYears >= 0) { ViewData["YearsError"] = "Invalid Years"; return View(); } else { ViewData["Years"] = goodYears; }
 
-            ViewData["Value"] = value;
-            ViewData["Rate"] = rate;
-            ViewData["Years"] = years;
-
-            ViewData["SimpleAmount"] = (value * rate * years) / 100;
+            ViewData["SimpleAmount"] = value + ((value * rate * years) / 100);
             ViewData["CompoundAmount"] = value * Math.Pow(1 + (rate / 100), years);
 
             return View();
