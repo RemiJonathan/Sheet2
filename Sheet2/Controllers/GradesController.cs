103d750b484f1edc27324ab042a8b609a8327bf3
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-09-08 20:49:47 -0400

    Final commit Completed Grades Controller and View

--- Sheet2/Controllers/GradesController.cs
+++ Sheet2/Controllers/GradesController.cs
@@ -13,5 +13,25 @@ namespace Sheet2.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            float gradesAccumulator = 0;
+            for (int i = 0; i < form.Count; i++)
+            {
+                if (float.TryParse(form[i], out float grade) && grade >= 0 && grade <= 100) gradesAccumulator += grade;
+                else { ViewData["Grade Error"] = " One of these grades is invalid, must be a number between 0 and 100."; return View(); }
+            }
+            float ngrade = gradesAccumulator / form.Count;
+            ViewData["NGrade"] = ngrade;
+
+            if (ngrade >= 0.0 && ngrade < 60) ViewData["CGrade"] = "F";
+            else if (ngrade >= 60.0 && ngrade < 70.0) ViewData["CGrade"] = "D";
+            else if (ngrade >= 70.0 && ngrade < 80.0) ViewData["CGrade"] = "C";
+            else if (ngrade >= 80.0 && ngrade < 90.0) ViewData["CGrade"] = "B";
+            else if (ngrade >= 90.0 && ngrade <= 100.0) ViewData["CGrade"] = "A";
+            return View();
+        }
     }
 }
